7c6c0947d3de2da93ee363ea7ad9f7d118d42eb1
Holger Woltersdorf <hw@hollo.me>
2018-04-07 17:19:03 +0200

    Add docker-compose test files

---
+++ tests/config/app.php
@@ -0,0 +1,5 @@
+<?php declare(strict_types=1);
+
+return [
+	'baseUrl' => 'http://localhost:8079/',
+];
---
+++ tests/config/servers.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+return [
+	[
+		'name'          => 'Redis-Server 1',
+		'host'          => 'readis_di_tests_redis',
+		'port'          => 6379,
+		'auth'          => null,
+		'timeout'       => 2.5,
+		'retryInterval' => 100,
+		'databaseMap'   => [],
+	],
+];
