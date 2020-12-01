6dd477eaba17872710fd325be4caf1b83ea5c2d7
Holger Woltersdorf <hw@hollo.me>
2020-12-01 15:57:09 +0100

    Fix local test setup

--- tests/config/servers.php
+++ tests/config/servers.php
@@ -3,7 +3,7 @@
 return [
 	[
 		'name'          => 'Redis-Server 1',
-		'host'          => 'readis_di_tests_redis',
+		'host'          => 'redis',
 		'port'          => 6379,
 		'auth'          => null,
 		'timeout'       => 2.5,
